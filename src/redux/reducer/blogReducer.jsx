aaf02b762e4cab156371bc84942c71b37661bdf2
Thupha41 <phatngo.040103@gmail.com>
2024-11-29 16:33:35 +0700

    Fetch rest blog api

--- src/redux/reducer/blogReducer.jsx
+++ src/redux/reducer/blogReducer.jsx
@@ -11,6 +11,24 @@ import {
   GET_BLOG_IMAGES_REQUEST,
   GET_BLOG_IMAGES_SUCCESS,
   GET_BLOG_IMAGES_FAILURE,
+  GET_BLOG_DETAIL_REQUEST,
+  GET_BLOG_DETAIL_SUCCESS,
+  GET_BLOG_DETAIL_FAILURE,
+  CREATE_COMMENT_REQUEST,
+  CREATE_COMMENT_SUCCESS,
+  CREATE_COMMENT_FAILURE,
+  GET_COMMENTS_REQUEST,
+  GET_COMMENTS_SUCCESS,
+  GET_COMMENTS_FAILURE,
+  DELETE_COMMENT_REQUEST,
+  DELETE_COMMENT_SUCCESS,
+  DELETE_COMMENT_FAILURE,
+  UPDATE_COMMENT_REQUEST,
+  UPDATE_COMMENT_SUCCESS,
+  UPDATE_COMMENT_FAILURE,
+  DELETE_BLOG_REQUEST,
+  DELETE_BLOG_SUCCESS,
+  DELETE_BLOG_FAILURE,
 } from "../action/blogAction";
 
 const initialState = {
@@ -24,6 +42,20 @@ const initialState = {
   blogImages: [],
   blogImagesLoading: false,
   blogImagesError: null,
+  blogDetail: null,
+  blogDetailLoading: false,
+  blogDetailError: null,
+  comments: [],
+  commentLoading: false,
+  commentError: null,
+  commentsLoading: false,
+  commentsError: null,
+  deleteCommentLoading: false,
+  deleteCommentError: null,
+  updateCommentLoading: false,
+  updateCommentError: null,
+  deleteBlogLoading: false,
+  deleteBlogError: null,
 };
 
 const blogReducer = (state = initialState, action) => {
@@ -122,6 +154,164 @@ const blogReducer = (state = initialState, action) => {
         ...state,
         blogImagesLoading: false,
         blogImagesError: action.payload,
+      };
+
+    // Blog Detail Cases
+    case GET_BLOG_DETAIL_REQUEST:
+      return {
+        ...state,
+        blogDetailLoading: true,
+        blogDetailError: null,
+      };
+
+    case GET_BLOG_DETAIL_SUCCESS:
+      return {
+        ...state,
+        blogDetailLoading: false,
+        blogDetail: action.payload,
+        blogDetailError: null,
+      };
+
+    case GET_BLOG_DETAIL_FAILURE:
+      return {
+        ...state,
+        blogDetailLoading: false,
+        blogDetail: null,
+        blogDetailError: action.payload,
+      };
+
+    // Comment Cases
+    case CREATE_COMMENT_REQUEST:
+      return {
+        ...state,
+        commentLoading: true,
+        commentError: null,
+      };
+
+    case CREATE_COMMENT_SUCCESS:
+      return {
+        ...state,
+        commentLoading: false,
+        comments: [...state.comments, action.payload],
+        blogDetail:
+          state.blogDetail?.id === action.payload.post
+            ? {
+                ...state.blogDetail,
+                comments_count: (state.blogDetail.comments_count || 0) + 1,
+              }
+            : state.blogDetail,
+        commentError: null,
+      };
+
+    case CREATE_COMMENT_FAILURE:
+      return {
+        ...state,
+        commentLoading: false,
+        commentError: action.payload,
+      };
+
+    // Get Comments Cases
+    case GET_COMMENTS_REQUEST:
+      return {
+        ...state,
+        commentsLoading: true,
+        commentsError: null,
+      };
+
+    case GET_COMMENTS_SUCCESS:
+      return {
+        ...state,
+        commentsLoading: false,
+        comments: action.payload,
+        commentsError: null,
+      };
+
+    case GET_COMMENTS_FAILURE:
+      return {
+        ...state,
+        commentsLoading: false,
+        comments: [],
+        commentsError: action.payload,
+      };
+
+    case DELETE_COMMENT_REQUEST:
+      return {
+        ...state,
+        deleteCommentLoading: true,
+        deleteCommentError: null,
+      };
+
+    case DELETE_COMMENT_SUCCESS:
+      return {
+        ...state,
+        deleteCommentLoading: false,
+        comments: state.comments.filter(
+          (comment) => comment.id !== action.payload.commentId
+        ),
+        blogDetail:
+          state.blogDetail?.id === action.payload.postId
+            ? {
+                ...state.blogDetail,
+                comments_count: Math.max(
+                  (state.blogDetail.comments_count || 0) - 1,
+                  0
+                ),
+              }
+            : state.blogDetail,
+        deleteCommentError: null,
+      };
+
+    case DELETE_COMMENT_FAILURE:
+      return {
+        ...state,
+        deleteCommentLoading: false,
+        deleteCommentError: action.payload,
+      };
+
+    case UPDATE_COMMENT_REQUEST:
+      return {
+        ...state,
+        updateCommentLoading: true,
+        updateCommentError: null,
+      };
+
+    case UPDATE_COMMENT_SUCCESS:
+      return {
+        ...state,
+        updateCommentLoading: false,
+        comments: state.comments.map((comment) =>
+          comment.id === action.payload.id ? action.payload : comment
+        ),
+        updateCommentError: null,
+      };
+
+    case UPDATE_COMMENT_FAILURE:
+      return {
+        ...state,
+        updateCommentLoading: false,
+        updateCommentError: action.payload,
+      };
+
+    case DELETE_BLOG_REQUEST:
+      return {
+        ...state,
+        deleteBlogLoading: true,
+        deleteBlogError: null,
+      };
+
+    case DELETE_BLOG_SUCCESS:
+      return {
+        ...state,
+        deleteBlogLoading: false,
+        blogs: state.blogs.filter((blog) => blog.id !== action.payload),
+        deleteBlogError: null,
+      };
+
+    case DELETE_BLOG_FAILURE:
+      return {
+        ...state,
+        deleteBlogLoading: false,
+        deleteBlogError: action.payload,
       };
 
     default:
